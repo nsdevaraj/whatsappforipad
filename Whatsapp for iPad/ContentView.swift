7ace0c6d76e0aed9fa01e37127b95b44c07c6080
devaraj <nsdevaraj@gmail.com>
2022-05-25 14:43:16 +0530

    Initial Commit

---
+++ Whatsapp for iPad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Whatsapp for iPad
+//
+//  Created by DEVARAJ N S on 5/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Whatsapp for iPad/Whatsapp_for_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whatsapp_for_iPadApp.swift
+//  Whatsapp for iPad
+//
+//  Created by DEVARAJ N S on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Whatsapp_for_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
